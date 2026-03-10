0cde84fdb1515e0f1746a0ce3e7ef1092db9c0a9
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-11 00:14:08 +0530

    onyx: Switch to dummy micharge service
    Change-Id: Id6aded7f9b551734b784bdf5d18a988cee88d243
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+# Micharge
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.micharge-service-dummy
+
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
